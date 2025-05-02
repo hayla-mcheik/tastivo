e0e62ae90f5952724728dd99fcc8bd58d9e424d8
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-05-03 00:43:08 +0300

    first commit

--- resources/js/Pages/Menu.vue
+++ resources/js/Pages/Menu.vue
@@ -564,12 +564,12 @@ const formatPrice = (price) => {
             </div>
             <h3 class="text-lg font-medium text-gray-700 mb-2">No products found</h3>
             <p class="text-gray-500 mb-4">Try selecting a different category</p>
-            <button 
-                @click="activeCategory = null"
+            <Link
+               href="/"
                 class="px-4 py-2 bg-black text-white rounded-lg hover:bg-black transition-colors"
             >
-                Show all products
-            </button>
+                Show All Categories
+          </Link>
         </div>
         
         <!-- Flying items container -->
