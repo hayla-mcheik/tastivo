5805ce881a8b2efcc3a08b92146233e05df61446
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-05-02 22:50:21 +0300

    first commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,7 @@ const pinia = createPinia();
 
 const i18n = createI18n({
     legacy: false,
-    locale: localStorage.getItem('locale') || "en-US", 
+    locale: "en-US", 
     fallbackLocale: "en-US",
     messages: {
         "en-US": english.messages,
